4af09e11dad66ec27392cab0576f7e4d5b88e127
FletcherCutting <admin@fletchercutting.au>
2024-02-15 22:18:56 +1100

    Add Client struct

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package scrygo
 
 import (
+	"net/http"
 	"time"
 )
 
@@ -8,3 +9,15 @@ const (
 	defaultBaseURL = "https://api.scryfall.com"
 	defaultTimeout = 5 * time.Second
 )
+
+// The client struct for managing requests to the Scryfall API.
+type Client struct {
+	// the base URL for requests to the Scryfall API
+	BaseURL string
+
+	// request timeout in seconds
+	Timeout int
+
+	// the http client used for requests
+	HTTPClient *http.Client
+}
